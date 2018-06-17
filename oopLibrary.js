e2559723623219213ca5b99d441844674e18cb43
jenniferhsueh <jenhsueh89@hotmail.com>
2018-06-17 04:24:13 +0000

    refactored addtrack

--- oopLibrary.js
+++ oopLibrary.js
@@ -18,13 +18,14 @@ function Track(title, rating, length) {
 
 Library.prototype.addPlaylist = function (playlist) {
   this.playlists.push(playlist)
-  console.log(`NEW PLAYLIST, ${playlist}, was added to ${this.name}`)
+  // console.log(`NEW PLAYLIST, ${playlist}, was added to ${this.name}`)
 }
 
 Playlist.prototype.addTrack = function (title, rating, length) {
-  let addNewTrack = new Track(title, rating, length)
-  this.tracks.push(addNewTrack)
-  // console.log(`NEW TRACK, ${title} ${length}, was added to ${this.name}`) 
+  this.tracks.push({title: title, rating: rating, length: length, });
+  // let addNewTrack = ({title, rating, length})
+  // this.tracks.push(addNewTrack)
+  console.log(`NEW TRACK, ${title}, was added to ${this.name}`) 
 }
 
 Playlist.prototype.totalDuration = function() {
@@ -46,7 +47,7 @@ Playlist.prototype.overallRating = function() {
 }
 
 const spotify = new Library("Spotify", "Jenn")
-console.log(spotify)
+// console.log(spotify)
 
 const summer = new Playlist("Summer2018")
 // console.log(newPlaylist)
@@ -55,7 +56,7 @@ const newTrack = new Track("Dance with me", 4, 240)
 // console.log(newTrack)
 
 spotify.addPlaylist("abgt250");
-console.log(spotify)
+// console.log(spotify)
 
 summer.addTrack("We're all we need", 5, 120);
 summer.addTrack("Black room boy", 1, 130)
